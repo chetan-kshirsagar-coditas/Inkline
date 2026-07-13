34288dafad03e10d60698fb25f6e45579fa55e04
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-07-13 14:01:20 +0530

    update user model

--- backend/python_be/app/models/user.py
+++ backend/python_be/app/models/user.py
@@ -6,9 +6,9 @@ from sqlalchemy.dialects.postgresql import UUID
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=func.now)
-    created_by = Column(UUID(as_uuid=True), ForeignKey("users.user_id"), nullable=False)
+    created_by = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime, default=func.now())
