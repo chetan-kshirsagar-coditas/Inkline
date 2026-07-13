b13400628679b0a26e474c1c7440c973b9d03202
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-07-13 16:16:23 +0530

    fix content foreign key

--- backend/python_be/app/models/attachment.py
+++ backend/python_be/app/models/attachment.py
@@ -7,7 +7,7 @@ from sqlalchemy.dialects.postgresql import UUID
 class Attachment(Base):
     __tablename__ = "attachments"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    content = Column(UUID(as_uuid=True), ForeignKey("content.id"), nullable=False)
-    attachment_url = Column(String, ForeignKey("documents.id"), nullable=False)
+    content = Column(UUID(as_uuid=True), ForeignKey("contents.id"), nullable=False)
+    attachment_url = Column(String, ForeignKey("documents.document_url"), nullable=False)
     is_public = Column(Boolean, nullable=False)
     uploaded_at = Column(DateTime, default=func.now())
