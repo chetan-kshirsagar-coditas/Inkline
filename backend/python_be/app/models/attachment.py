6aa294660d8b425b2e82387d1b60223362d51473
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-07-13 15:06:42 +0530

    create attachment table

---
+++ backend/python_be/app/models/attachment.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, func, DateTime, Boolean, ForeignKey
+from app.core.database import Base
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class Attachment(Base):
+    __tablename__ = "attachments"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    content = Column(UUID(as_uuid=True), ForeignKey("content.id"), nullable=False)
+    attachment_url = Column(String, ForeignKey("documents.id"), nullable=False)
+    is_public = Column(Boolean, nullable=False)
+    uploaded_at = Column(DateTime, default=func.now())
