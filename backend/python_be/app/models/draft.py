51049f3f9df05b39588b3b4767203508ff39692c
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-07-13 15:14:15 +0530

    fix draft.py

--- backend/python_be/app/models/draft.py
+++ backend/python_be/app/models/draft.py
@@ -9,7 +9,7 @@ class Draft(Base):
     __tablename__ = "drafts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    content = Column(UUID(as_uuid=True), ForeignKey("content.id"), nullable=False)
+    content = Column(UUID(as_uuid=True), ForeignKey("contents.id"), nullable=False)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, server_default=func.now())
     is_submitted = Column(Boolean, default=False)
