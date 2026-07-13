b7769a60404aa997c61e1834c45e773c58b3943e
Priyanshu Deshmukh <priyanshu.deshmukh@coditas.com>
2026-07-13 13:29:20 +0530

    be/config/initial configurations

---
+++ backend/python_be/core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+
+class Setting(BaseSettings):
+
+    DB_USERNAME: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+    
+    @property
+    def DB_URL(self):
+        return f"postgresql+psycopg2://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config={
+        'env_file': ".env"
+    }
+
+    
+settings = Setting()
